dd9ab8a18d876851a178ed202a8c4aaaeb11e234
Gaurang Singh <gsingh8003@gmail.com>
2023-10-13 14:10:40 +0530

    Polygon Mumbai Testnet functionalities added

--- Context/constants.js
+++ Context/constants.js
@@ -3,5 +3,5 @@
 import nftMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
 
 export const NFTMarketplaceAddress =
-  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  "0x8f69f76fC6C21Df4CD66041199Fc12Bf20268AC8";
 export const NFTMarketplaceABI = nftMarketplace.abi;
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,14 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  // To setup the Polygon Testnet
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/Jd-14noFCTcppkndVAtnQ0ZKTYvhrhtm",
+      accounts: [
+        `0x${"0024f94a8d364a030196a6239caeca74a6d121a74e64200973fb0231e8f6c363"}`,
+      ],
+    },
+  },
 };
